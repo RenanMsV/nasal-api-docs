ad1f8d66cecf690069c2566d12d74a429eb55a4f
RenanMsV <r.delaneza@gmail.com>
2026-05-12 20:50:32 -0300

    feat: add markdown and build args
    - `--markdown` argument tells it to output markdown files that can be used with MkDocs.
    - `--build` argument tells it to build the MkDocs site.

--- nasal_api_docs/__main__.py
+++ nasal_api_docs/__main__.py
@@ -29,6 +29,8 @@ Usage:
 import argparse
 import sys
 from pathlib import Path
+from mkdocs.config import load_config as mkdocs_load_config  # type: ignore
+from mkdocs.commands.build import build as mkdocs_build
 
 from nasal_api_docs import NasalAPI
 from .logger import get_logger
@@ -69,6 +71,21 @@ def _parse_args(argv: list[str] | None = None) -> argparse.Namespace:
         dest="json",
         help="Will output a generated json file.",
     )
+    parser.add_argument(
+        "--markdown",
+        action="store_true",
+        dest="markdown",
+        help="Will output generated markdown files (MkDocs).",
+    )
+    parser.add_argument(
+        "--build",
+        action="store_true",
+        dest="build",
+        help=(
+            "Will build files if necessary. "
+            "Useful if you need to build: [MkDocs, ...]"
+        ),
+    )
 
     args = parser.parse_args(argv)
     return args
@@ -84,21 +101,39 @@ def main(argv: list[str] | None = None) -> int:
     should_output_all = not any([
         args.html,
         args.json,
+        args.markdown,
     ])
 
     try:
         nasal_api = NasalAPI(fg_root_dir, output_dir)
 
         fg_version = nasal_api.get_fg_version()
-        logger.info("Found FlightGear version %s", fg_version)
+        logger.info("Generating outputs for FlightGear version %s", fg_version)
 
-        if should_output_all is True or args.html:
+        if should_output_all or args.html:
             html_file = nasal_api.generate_html()
-            logger.info("Generated HTML at %s", html_file)
+            logger.info("Generated HTML file at %s", html_file)
 
-        if should_output_all is True or args.json:
+        if should_output_all or args.json:
             json_file = nasal_api.generate_json_tree()
             logger.info("Generated JSON tree at %s", json_file)
+
+        if should_output_all or args.markdown:
+            markdown_out_dir = nasal_api.generate_markdown()
+            logger.info("Generated MARKDOWN files at %s", markdown_out_dir)
+
+        if args.build:
+            mkdocs_config = mkdocs_load_config()
+            # Replace $fg_version in the mkdocs_config with the FlightGear version
+            mkdocs_config.site_name = mkdocs_config.site_name.replace(
+                "$fg_version",
+                fg_version
+            )
+            # Modify site and docs dir to be inside the output_dir to respect -o arg
+            mkdocs_config.site_dir = str(output_dir / "mkdocs" / "site")
+            mkdocs_config.docs_dir = str(output_dir / "mkdocs" / "docs")
+            logger.info("Building MARKDOWN with MkDocs at %s", mkdocs_config.site_dir)
+            mkdocs_build(mkdocs_config)
 
     except FileNotFoundError as e:
         logger.error("Missing file or directory: %s", e)
